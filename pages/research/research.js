827f50fddd5932e3f71a25d90cccfd7b45bdf033
Jerry-K5050 <jerry5058@outlook.com>
2026-05-19 08:18:27 -0400

    reformated the index page and change field-of-study to research

---
+++ pages/research/research.js
@@ -0,0 +1,10 @@
+const logoImg = document.querySelector(".logo");
+
+function logoToHomepage(){
+    logoImg.addEventListener("click", () => {
+        window.location.href = "../../index.html";
+    });
+}
+
+
+logoToHomepage();
